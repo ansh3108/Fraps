b0827d247d91d0eb604e29b5864e9aff6c41caf0
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-19 15:37:08 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+const App = () => {
+return(
+  <div>
+    3D Developer Portfolio
+  </div>
+)
+}
+
+export default App
